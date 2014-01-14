4396e8c0623b1f2651ff3a5c11c603c38a0fadf3
Ryan Langley <rjmlangley@gmail.com>
2014-01-13 22:01:14 -0500

    Added comments.

--- src/main/java/com/rvgames/app/App.java
+++ src/main/java/com/rvgames/app/App.java
@@ -6,6 +6,9 @@ package com.rvgames.app;
  */
 public class App 
 {
+    /**
+     * @param args
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
